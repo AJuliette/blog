c470dd49e9ab195c6380abec188f3a94b581b0ec
Juliette Audema <juliette@capsens.eu>
2021-03-22 20:03:01 +0100

    Add section welcome

--- components/Welcome.tsx
+++ components/Welcome.tsx
@@ -1,12 +1,7 @@
-import React, { useContext } from 'react';
-import { FaEnvelope, FaGithub, FaTwitter, FaMugHot } from 'react-icons/fa';
+import React from 'react';
 import Image from 'next/image';
 
-import { ImTextWidth } from 'react-icons/im';
 import styles from './Welcome.module.scss';
-import PreferencesContext from '../contexts/PreferencesContext';
-import Slider from './Slider';
-import { useScroll } from '../hooks/useScroll';
 
 const ImgProfilePic = () => (
   <div className={styles.pictureContainer}>
@@ -14,93 +9,19 @@ const ImgProfilePic = () => (
   </div>
 );
 
-const Welcome = () => {
-  const { wideCodeBlock, toggleWideCodeBlocks } = useContext(
-    PreferencesContext
-  );
-  const { atTopScroll, scrollingUp } = useScroll();
-
-  return (
-    <div>
-      <ImgProfilePic />
-      <header>
-        <h1>Welcome !</h1>
-        <h3>Les élucubrations d'un développeur fatigué</h3>
-        <p>
-          Développeur, ludiste, admin sys, grimpeur et cuisinier à mes heures.
-          Je suis curieux de tout, tout le temps. J’aime découvrir de nouvelles
-          choses et les partager.
-        </p>
-      </header>
-      <section>
-        <h2>Me contacter</h2>
-        <ul>
-          <li>
-            <FaEnvelope />
-            <span className={styles.space} />
-            <a
-              href="mailto:zaratan@hey.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              zaratan@hey.com
-            </a>
-          </li>
-          <li>
-            <FaGithub />
-            <span className={styles.space} />
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://github.com/zaratan"
-            >
-              zaratan
-            </a>
-          </li>
-          <li>
-            <FaTwitter />
-            <span className={styles.space} />
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://twitter.com/zaratan"
-            >
-              @zaratan
-            </a>
-          </li>
-        </ul>
-      </section>
-      <section>
-        <h2>Me soutenir</h2>
-        <ul>
-          <li>
-            <FaMugHot />
-            <span className={styles.space} />
-            <a
-              href="https://ko-fi.com/zaratan"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Ko-Fi
-            </a>
-          </li>
-        </ul>
-      </section>
-      <section>
-        <h2>Options</h2>
-        <ul>
-          <li>
-            <ImTextWidth />
-            <span className={styles.space} />
-            <span>
-              Blocs de code larges :{' '}
-              <Slider active={wideCodeBlock} action={toggleWideCodeBlocks} />
-            </span>
-          </li>
-        </ul>
-      </section>
-    </div>
-  );
-};
+const Welcome = () => (
+  <section className={styles.profileContainer}>
+    <ImgProfilePic />
+    <header className={styles.header}>
+      <p className={styles.title}>Hi, I'm Juliette</p>
+      <p className={styles.subtitle}>Thanks for stepping by !</p>
+      <p>
+        I'm a back-end Ruby on Rails developer. I'm curious about a lot of
+        things in web development. I love knitting and embroidery. Here to share
+        my journey.
+      </p>
+    </header>
+  </section>
+);
 
 export default Welcome;
